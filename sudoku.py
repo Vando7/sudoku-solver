c0b4eb43b16a44c93ba695e71a0b9747b87b418f
Vankdo7 <IPMihaylov@outlook.com>
2021-10-21 12:26:37 +0300

    cosmetic changes

--- sudoku.py
+++ sudoku.py
@@ -1,8 +1,8 @@
 import sys
 import os
 from array import *
-from copy import copy, deepcopy
 sys.setrecursionlimit(2000)
+
 
 class Sudoku(object):
     path = ''
@@ -20,22 +20,24 @@ class Sudoku(object):
     def _printBoard(self, board):
         for i in range(9):
             for j in range(9):
-                if(j % 3 == 0 and j != 0): print("| ",end='')
+                if(j % 3 == 0 and j != 0):
+                    print("| ", end='')
                 print(board[i][j], end=' ')
-            
+
             print('')
-            if(i % 3 == 2 and i != 8): 
+            if(i % 3 == 2 and i != 8):
                 for j in range(21):
                     if(j == 6 or j == 14):
-                        print('+',end='')
+                        print('+', end='')
                     else:
-                        print('-',end='')
+                        print('-', end='')
                 print('')
+
 
     def loadBoard(self, path):
         if(path == None):
             print("loadBoard: no 'file' argument given, terminating.")
-            return 
+            return
 
         print("Loading file.")
         file = open(path, 'r')
@@ -44,7 +46,7 @@ class Sudoku(object):
         row = 0
         for line in lines:
             line = line.strip()
-            
+
             col = 0
             for a in line:
                 self.board[row][col] = int(a)
@@ -55,119 +57,119 @@ class Sudoku(object):
         print("Done. Closing " + path)
         file.close()
         return True
-    
-    
+
+
     def cellValues(self, board):
         if self.isOpen == False:
             print("No board loaded.")
             return
-        
+
         cells = [[0 for i in range(9)] for i in range(9)]
-        
+
         for i in range(9):
             for j in range(9):
                 if board[i][j] == 0:
-                    cells[i][j] = self.possibleValues(board,i,j)
+                    cells[i][j] = self.possibleValues(board, i, j)
                 else:
                     cells[i][j] = []
-    
+
         return cells
-    
-    
+
+
     def fillHiddenSingles(self, board):
         if self.isOpen == False:
             print("No board loaded.")
             return
-        
+
         cells = self.cellValues(board)
-        
+
         # Fill hidden singles per column
         cellChanged = True
         while cellChanged:
             cellChanged = False
-            
+
             for i in range(9):
                 countEncounters = [0 for z in range(10)]
                 for j in range(9):
                     for k in range(len(cells[j][i])):
                         countEncounters[cells[j][i][k]] += 1+1-1
-                        
+
                 hiddenSingles = []
                 for j in range(10):
                     if countEncounters[j] == 1:
                         hiddenSingles.append(j)
-                        
+
                 for value in hiddenSingles:
                     for j in range(9):
                         if(value in cells[j][i]):
                             board[j][i] = value
                             cells = self.cellValues(board)
                             cellChanged = True
-        
+
         # Fill hidden singles per column
         cellChanged = True
         while cellChanged:
             cellChanged = False
-            
+
             for j in range(9):
                 countEncounters = [0 for z in range(10)]
                 for i in range(9):
                     for k in range(len(cells[j][i])):
                         countEncounters[cells[j][i][k]] += 1
-                        
+
                 hiddenSingles = []
                 for i in range(10):
                     if countEncounters[i] == 1:
                         hiddenSingles.append(i)
-                        
+
                 for value in hiddenSingles:
                     for i in range(9):
                         if(value in cells[j][i]):
                             board[j][i] = value
                             cells = self.cellValues(board)
-                            cellChanged = True        
-        
+                            cellChanged = True
+
         return
-    
+
 
     def possibleValues(self, board, row, col):
         if self.isOpen == False:
             print("No board loaded.")
             return
-        
+
         usedValues = [0 for i in range(10)]
-        
-        for i in range(0,9):
+
+        for i in range(0, 9):
             if(board[i][col] != 0):
                 usedValues[board[i][col]] = 1
             if(board[row][i] != 0):
                 usedValues[board[row][i]] = 1
-        
+
         quadrantRow = 1
         quadrantCol = 1
-        
+
         if(row < 3):
             quadrantRow = 0
         if(row > 5):
             quadrantRow = 2
-        quadrantRow *=3
-        
+        quadrantRow *= 3
+
         if(col < 3):
             quadrantCol = 0
         if(col > 5):
             quadrantCol = 2
         quadrantCol *= 3
-        
+
         for i in range(quadrantRow, quadrantRow+3):
             for j in range(quadrantCol, quadrantCol+3):
                 if(board[i][j] != 0):
                     usedValues[board[i][j]] = 1
-                    
+
         result = []
-        for i in range(1,10):
+        for i in range(1, 10):
             if(usedValues[i] == 0):
                 result.append(i)
-        
+
         return result
 
 
@@ -175,37 +177,36 @@ class Sudoku(object):
         if self.isOpen == False:
             print("No board loaded.")
             return
-        
+
         blankRow = -1
         blankCol = -1
 
-        for i in range(0,9):
-            for j in range(0,9):
+        for i in range(0, 9):
+            for j in range(0, 9):
                 if board[i][j] == 0:
                     blankRow = i
                     blankCol = j
                     break
-                
+
             if blankRow != -1:
                 break
-        
-        
+
         if blankRow == -1:
             print("Solved board:")
             self._printBoard(board)
             self.writeBoard()
             return True
-        
+
         allowedValues = self.possibleValues(board, blankRow, blankCol)
 
         for i in range(0, len(allowedValues)):
             board[blankRow][blankCol] = allowedValues[i]
-            
+
             if(self._solve(board)):
                 return True
-            
+
             board[blankRow][blankCol] = 0
-        
+
         return False
 
 
@@ -214,13 +215,13 @@ class Sudoku(object):
             print("No board loaded.")
             return
         self._solve(self.board)
-        
+
 
     def smart_solve(self):
         if self.isOpen == False:
             print("No board loaded.")
             return
-        
+
         self.fillHiddenSingles(self.board)
         self._solve(self.board)
 
@@ -242,12 +243,12 @@ class Sudoku(object):
 
         self.isOpen = True
         self.path = path
-        
-    
+
+
     def writeBoard(self):
         inputFileName = self.path.split('.')[0]
         print('\nWriting result to ' + inputFileName + '-solved.txt')
-        
+
         resultFile = open(inputFileName + '-solved.txt', 'w')
         for i in range(9):
             for j in range(9):
@@ -257,8 +258,6 @@ class Sudoku(object):
         print("Done, closing.")
 
 
-
-
 def main():
     if(len(sys.argv) != 2):
         print("No arguments given.")
@@ -266,17 +265,17 @@ def main():
         return 0
 
     sudoku = Sudoku(sys.argv[1])
-    
+
     if(sudoku.isOpen == False):
         return
-    
+
     print("Input board:\n")
     sudoku.printBoard()
     print("\nSolving . . . \n")
-    sudoku.smart_solve()  
-    
+    sudoku.smart_solve()
+
     return 0
 
- 
+
 if __name__ == "__main__":
     main()
